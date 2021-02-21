1a8ff464e0162c5f1d90ca7d675b85095ed66682
natsuokawai <kawai.natsuo@gmail.com>
2021-02-21 20:19:09 +0900

    Add function read_file

--- tokenize.c
+++ tokenize.c
@@ -249,3 +249,38 @@ Token *tokenize(char *input) {
     return head.next;
 }
 
+static char *read_file(char *path) {
+    FILE *fp;
+
+    if (strcmp(path, "-") == 0) {
+        fp = stdin;
+    } else {
+        fp = fopen(path, "r");
+        if (!fp) {
+            error("cannot open %s: %s", path, strerror(errno));
+        }
+    }
+
+    char *buf;
+    size_t buflen;
+    FILE *out = open_memstream(&buf, &buflen);
+
+    for (;;) {
+        char buf2[4096];
+        int n = fread(buf2, 1, sizeof(buf2), fp);
+        if (n == 0) {
+            break;
+        }
+        fwrite(buf2, 1, n, out);
+    }
+
+    if (fp != stdin) {
+        fclose(fp);
+    }
+    fflush(out);
+    if (buflen == 0 || buf[buflen - 1] == '\n') {
+        fputc('\n', out);
+    }
+    fclose(out);
+    return buf;
+}
